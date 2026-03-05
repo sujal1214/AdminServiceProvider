805021d32f08107a5eff2e3c1fa84604cf18f878
Sujal Shinde <sujalsunilshinde333@gmail.com>
2026-03-05 20:47:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/adminserviceprovider/LoginActivity.kt
@@ -0,0 +1,37 @@
+package com.example.adminserviceprovider
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.OnApplyWindowInsetsListener
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.adminserviceprovider.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.enableEdgeToEdge()
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        binding.dontHaveAccountButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(
+            findViewById<View?>(R.id.main),
+            OnApplyWindowInsetsListener { v: View?, insets: WindowInsetsCompat? ->
+                val systemBars = insets!!.getInsets(WindowInsetsCompat.Type.systemBars())
+                v!!.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            })
+    }
+}
---
+++ app/src/main/java/com/example/adminserviceprovider/SignUpActivity.kt
@@ -0,0 +1,39 @@
+package com.example.adminserviceprovider
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.adminserviceprovider.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+    private val binding: ActivitySignUpBinding by lazy {
+        ActivitySignUpBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        binding.createAccountButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        binding.alreadyHaveAccountButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val locationList = arrayOf("Kolad","Roha","Pale Khurd","Mahad","Mangaon")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, locationList)
+        val autoCompleteTextView = binding.listOfLocation
+        autoCompleteTextView.setAdapter(adapter)
+
+    }
+}
